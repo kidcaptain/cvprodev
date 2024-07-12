7289b983731ba78bae758b428386c7adfcb63f27
hunterjul007 <93106837+hunterjul007@users.noreply.github.com>
2024-07-12 11:57:43 +0100

    update

--- components/builder/Step-3.vue
+++ components/builder/Step-3.vue
@@ -80,10 +80,10 @@ const removeSaved = (item: number, index: number) => {
         }}</AccordionTrigger>
         <AccordionContent class="md:pl-5">
           <div class="space-y-2">
-            <div class="overflow-y-auto max-h-52 flex-col-reverse flex">
+            <div class="flex flex-col-reverse overflow-y-auto max-h-52">
               <div
                 v-for="(save, index) in item.datas"
-                class="my-2 border-b gap-5 p-2 cursor-pointer border-secondary"
+                class="gap-5 p-2 my-2 border-b cursor-pointer border-secondary"
               >
                 <div
                   v-if="itemIndex == 0"
@@ -139,7 +139,51 @@ const removeSaved = (item: number, index: number) => {
                     <span class="font-semibold">
                       Period:</span
                     >
-                    {{ save.start_date }} /  {{ save.start_date }}
+                    {{ save.start_date }} /  {{ save.end_date }}
+                  </h3>
+                  <!-- <div
+                    :class="
+                      save.company || save.grade || save.type ? 'border-r' : ''
+                    "
+                    class="flex items-center justify-end px-2 text-end border-secondary/50"
+                  >
+                    <span>{{ save.company || save.grade || save.type }}</span>
+                  </div> -->
+                </div>
+                <div
+                  v-if="itemIndex == 3"
+                  class="bg-secondary/5 hover:bg-secondary/10"
+                >
+                  <h3
+                    :class="
+                      save.company || save.grade || save.type ? 'border-r' : ''
+                    "
+                    class="border-secondary/50"
+                  >
+                    <span class="font-semibold">Institution:</span>
+                    {{ save.title }}
+                  </h3>
+                  <h3
+                    :class="
+                      save.company || save.grade || save.type ? 'border-r' : ''
+                    "
+                    class="border-secondary/50"
+                  >
+                    <span class="font-semibold">
+                      Award or honor obtained:</span
+                    >
+                    {{ save.award }}
+                  </h3>
+                  <h3
+                    :class="
+                      save.company || save.award || save.type ? 'border-r' : ''
+                    "
+                    class="border-secondary/50"
+                  >
+                    <span class="font-semibold">
+                      Date:</span
+                    >
+                    {{ save.start_date }}
                   </h3>
                   <!-- <div
                     :class="
@@ -154,29 +198,24 @@ const removeSaved = (item: number, index: number) => {
                   v-if="itemIndex == 1"
                   class="bg-secondary/5 hover:bg-secondary/10"
                 >
-                 
                   <h3
-                   
-                    
                   >
                     <span class="font-semibold">Hobbies or interests:</span> {{  save.title || save.grade || save.type }}
                   </h3>
                 
                 </div>
                 <div class="flex items-center justify-end gap-5 text-end">
-               
                   <Button
                     @click="removeSaved(itemIndex, index)"
                     variant="outline"
                     class="p-1 bg-secondary hover:border-secondary h-fit hover:text-secondary"
                     type="button"
                   >
-                    <Trash class="size-4 text-white hover:text-secondary" />
+                    <Trash class="text-white size-4 hover:text-secondary" />
                   </Button>
                 </div>
               </div>
             </div>
-
             <component
               :is="item.form"
               @submit="(value: any) => item?.datas?.push(value)"
